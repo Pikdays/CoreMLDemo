362f3d20e3491474e04ce45ec4787111795c9393
Pikdays <byt.quan@gmail.com>
2021-03-21 21:13:22 +0800

    Initial Commit

---
+++ Shared/CoreMLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLDemoApp.swift
+//  Shared
+//
+//  Created by Pikdays on 2021/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
